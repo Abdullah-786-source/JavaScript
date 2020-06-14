363d1d4a1adda9db1e8a591b4056bc6ab938bc24
Abdullah Ali <info.abdullah1@gmail.com>
2020-06-14 20:59:26 +0500

    Java Script

---
+++ app.js
@@ -0,0 +1,274 @@
+// // Chater # 1
+
+// // Task # 1
+// alert("Weclome!!");
+
+// // Task # 2
+
+// alert ("Error! Please enter a valid Password", "Any Message");
+
+// // Task # 3
+
+// window.alert("Welcome to JS Land...\nHappy Coding!");
+
+// // Task # 4
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// // Chatper # 2
+// // Task # 1
+
+// var username;
+
+// // Task # 2
+
+// var myName = "Syed Muhammad Abdullah Ali";
+
+// // Take # 3
+
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// // Task # 4
+// var name;
+// var age;
+// var certificate;
+
+// alert("Abdullah");
+// alert("29 Years Old");
+// alert("Certificate Mobile Application Developement");
+
+// //Task # 5
+// var pizza ="PIZZA";
+// alert(pizza.slice(0,5)+"\n"+pizza.slice(0,4)+"\n"+pizza.slice(0,3)+"\n"+pizza.slice(0,2)+"\n"+pizza.slice(0,1));
+
+// //Task # 6
+// var email = "example@email.com";
+// alert("My Email Address is "+ email);
+
+
+// //Task # 7
+
+// var book;
+// book = "A smarter way to learn JavaScript";
+// alert(book);
+
+// //Task # 8
+
+// document.getElementById("output").innerHTML = "Yah! I can write HTML content through Javascript";
+
+// //Task # 9
+
+// var a = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(a);
+
+// // chapter # 3
+// // Task # 1
+
+// var age;
+// alert("I am " + age + " Years old");
+
+// // Task # 2
+// var N;
+// N = 14;
+
+// alert("You have visited this site " + N + " times");
+
+// // Task # 4
+// var birthYear;
+// birthYear = 1990;
+// document.getElementById("byear").innerHTML = "My birth year is " + birthYear;
+// document.getElementById("btype").innerHTML = "Data type of my declared variable is " + (typeof birthYear);
+
+// // Task # 5
+//  var vName = "John Doe";
+//  var pTtitle = "T-shirt";
+//  var q = 5;
+
+
+// document.getElementById("order").innerHTML = "<b> " + vName + " </b>" + "ordered <b>" + q + " " + pTtitle + "</b>(s) on XYZ Clothing store";
+
+// // Chapter # 4
+
+// // Task # 1
+
+// var a, b, c;
+
+// //Task # 2
+// // var 2rest;
+// // var #123;
+// // var %Ab;
+// // var 123;
+// // var abc'123;
+
+// var abc;
+// var ab12;
+// var _res;
+// var $12;
+// var abc_123;
+
+// //Task # 3
+// document.getElementById("hd").innerHTML = "Rules of naming JS varibale";
+
+// document.getElementById("para").innerHTML ="Variable names can only contain, numbers, $ and _. For example: $my_1stVariable <br/> Variable must begin with a letter, $ or _. For Example: $name, _name, _name or name </br> Variable names are case sensitive <br/> Variable names should not be JS keywords";
+
+// // Chapter # 5
+// // Task # 1
+
+// var a = prompt("Enter a number");
+// var b = prompt("Enter another number");
+// c = a + b;
+// document.getElementById("add").innerHTML = "Sum of " + a + " and " + b + " is " + c;
+
+// // Task # 2
+
+// var a = prompt("Enter a number");
+// var b = prompt("Enter another number");
+// c = a - b;
+// document.getElementById("sub").innerHTML = "Subtraction of " + a + " and " + b + " is " + c;
+
+// var a = prompt("Enter a number");
+// var b = prompt("Enter another number");
+// c = a * b;
+// document.getElementById("mul").innerHTML = "Multification of " + a + " and " + b + " is " + c;
+
+// var a = prompt("Enter a number");
+// var b = prompt("Enter another number");
+// c = a / b;
+// document.getElementById("divi").innerHTML = "Division of " + a + " and " + b + " is " + c;
+
+// var a = prompt("Enter a number");
+// var b = prompt("Enter another number");
+// c = a % b;
+// document.getElementById("mod").innerHTML = "Modulus of " + a + " and " + b + " is " + c;
+
+
+// //Task # 3
+
+// var b;
+
+// document.getElementById("math1").innerHTML = "Value after variable declaration is " + b + "<br>";
+
+// b = 5;
+// document.getElementById("math2").innerHTML = "Initial value " + b + "<br>";
+// b = b+1;
+// document.getElementById("math3").innerHTML = "Value after increment is " + b + "<br>";
+// b = b+7;
+// document.getElementById("math4").innerHTML = "Value after addition is " + b + "<br>";
+// b = b - 1;
+// document.getElementById("math5").innerHTML = "Value after decrement is " + b + "<br>";
+// b = b % 3;
+// document.getElementById("math6").innerHTML = "The remainder is " + b + "<br>";
+
+// // Task # 4
+
+// var a;
+// a  = 600;
+
+// b = 600 * 5;
+// document.getElementById("ticket").innerHTML = "Total cost to buy 5 tickets to a movie is " + b + "PKR";
+
+// // Task # 5
+// var text="";
+// var i;
+// var a = 4;
+// var c;
+// for (i = 0; i <= 10; i++) {
+//     c = a * i;
+//     text += a + "x" + i + "=" + c + "<br>";
+//     }
+// document.getElementById("demo").innerHTML = text;
+
+// // Task # 6
+// var c = 25;
+// var f = 50;
+
+// c = (f -32)* 52/9;
+// f = (c * 9/5) + 32;
+
+// document.getElementById("temp").innerHTML = c + "°C is " + f + "°F";
+// document.getElementById("temp1").innerHTML = f + "°F is " + c + "°C";
+
+// // Task # 7
+// var p1 = 650;
+// var q1 = 3;
+// var p2 = 100;
+// var q2 = 7;
+// var s = 100;
+// var total = 0;
+
+// total = (p1*q1) + (p2*q2) + s;
+// document.getElementById("cart").innerHTML = "Price of item 1 is " + p1 + "<br>" + "Quantity of item 1 is " + q1 + "<br>" + "Price of item 2 is" + p2 + "<br>" + "Quntity of item 2 is " + q2 + "<br>" + "Shipping Charges " + s;
+// document.getElementById("total").innerHTML = "Total cost of your order is " + total;
+
+// //Task # 8
+// var Tmarks = 980;
+// var Omarks = 804;
+// var per = (Omarks / Tmarks)*100
+
+// document.getElementById("marks").innerHTML = "Total marks: " + Tmarks + "<br>" + "Marks Obtained: " + Omarks + "<br>" + "Percentage: " + per + "%";
+
+// // Task # 9
+// var usdpkr = 104.80;
+// var srpkr = 28;
+// var curr = 0;
+
+// curr = (10 * usdpkr) + (25 * srpkr);
+// document.getElementById("curr").innerHTML = "Total Currency in PKR: " + curr;
+
+// // Task # 10
+
+// var a = 10;
+// var b = 0;
+
+// b = (((a+5)*10)/2)
+
+// document.getElementById("art").innerHTML = "Answer: " + b;
+
+// //Task # 11
+// var cy = 2016;
+// var by = 1992;
+// var ag = 0;
+
+// ag = cy-by;
+
+// document.getElementById("age").innerHTML = "Current Year: " + cy + "<br>" + "Birth Year: " + by + "<br>" + "Your Age is: " + ag +"<br>"; 
+
+// // Task # 11
+// var rc = 20;
+// var pie = 3.142;
+// var cf = 0;
+// var area = 0;
+
+// cf = (2*pie*rc);
+// area = (pie* (Math.pow(rc,2)));
+// document.getElementById("geo").innerHTML = "Radius of a circle: " + rc + "<br>" + "The circumference is: "+ cf + "<br>" + "The area is: " + area;
+
+
+// // Task # 12
+// fv = "Chips"
+// cage = 15;
+// mage = 65;
+// spd = 3;
+// rs = (mage - cage) * spd;
+
+// document.getElementById("supply").innerHTML = "Favourtie Snack: " + fv + "<br>" + "Current Age: " + cage + "<br>" + "Estimated Maximum Age: " + mage + "<br>" + "Amount of snacks per day: " + spd + "<br>" + "You will need " + rs + " " + fv + " " + "to last you until ripe old age of " + mage;
+
+// Chapter 6 to 9
+// Task # 1
+var a = 10;
+document.getElementById("value0").innerHTML = "The value of a is " + a;
+
+a = ++a;
+document.getElementById("value1").innerHTML = "The value of ++a is " + a + "<br>" + "Now the value of a is: " + a;
+
+a = a++;
+document.getElementById("value2").innerHTML = "The value of a++ is " + a + "<br>" + "Now the value of a is: " + a;
+
+a = --a;
+document.getElementById("value3").innerHTML = "The value of --a is " + a + "<br>" + "Now the value of a is: " + a;
+
+a = a--;
+document.getElementById("value4").innerHTML = "The value of a-- is " + a + "<br>" + "Now the value of a is: " + a;
